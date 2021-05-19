607b01ccc0fa2c8c4408935a32a863e283ecd6df
Adithya Vijay K <adithyavj@gmail.com>
2021-05-19 23:31:49 +0530

    Added A New Controller
    Create new PeopleController and a model
    named person and did sample get,getbyid,
    post,put,delete requests.
    Since The model contains static values,
    only get and getbyid methods will work.

---
+++ APIDemo/Controllers/PeopleController.cs
@@ -0,0 +1,51 @@
+﻿using APIDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIDemo.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Adithya", LastName = "Vijay", Id = 1 });
+            people.Add(new Person { FirstName = "Arjun", LastName = "TT", Id = 2 });
+            people.Add(new Person { FirstName = "Vishnu", LastName = "K", Id = 3 });
+        }
+
+        // GET: api/People
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            // We use lambda expression here for - person.id==id that is passed in get command.
+            return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/People
+        public void Post(Person val)
+        {
+            people.Add(val);
+        }
+
+        // PUT: api/People/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
